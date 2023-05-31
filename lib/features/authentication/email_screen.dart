7373cc16d928c7c97d138a79ba72717af04fe660
shinung-han <hanshinung@gmail.com>
2023-05-31 21:10:14 +0900

    4.5 FormButton

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -1,1 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone_2/constants/gaps.dart';
+import 'package:tiktok_clone_2/constants/sizes.dart';
+import 'package:tiktok_clone_2/features/authentication/widgets/form_button.dart';
 
+class EmailScreeen extends StatefulWidget {
+  const EmailScreeen({super.key});
+
+  @override
+  State<EmailScreeen> createState() => _UsernameScreenState();
+}
+
+class _UsernameScreenState extends State<EmailScreeen> {
+  final TextEditingController _usernameController = TextEditingController();
+
+  String _username = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Sign up',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              'What is your email?',
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                hintText: 'email',
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v28,
+            FormButton(disabled: _username.isEmpty)
+          ],
+        ),
+      ),
+    );
+  }
+}
