c3b12e84b18bc99b81016fb7de49ab54aab43409
shinung-han <hanshinung@gmail.com>
2023-06-06 12:36:07 +0900

    11.4 code challenge

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -11,27 +11,65 @@ class ChatDetailScreen extends StatefulWidget {
 }
 
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  bool _isWriting = false;
+
+  void _onSendTap() {}
+
+  void _onStartWriting() {
+    setState(() {
+      _isWriting = true;
+    });
+  }
+
+  String _onSubmitted(String val) {
+    print(val);
+    setState(() {
+      _isWriting = false;
+    });
+    return val;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const ListTile(
+        title: ListTile(
           contentPadding: EdgeInsets.zero,
           horizontalTitleGap: Sizes.size8,
-          leading: CircleAvatar(
-            radius: Sizes.size24,
-            foregroundImage: NetworkImage(
-                'https://avatars.githubusercontent.com/u/118904460?v=4'),
-            child: Text('신웅'),
+          leading: Stack(
+            children: [
+              const CircleAvatar(
+                radius: Sizes.size24,
+                foregroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/118904460?v=4'),
+                child: Text('신웅'),
+              ),
+              Positioned(
+                right: -3,
+                bottom: -3,
+                child: Container(
+                  width: Sizes.size20,
+                  height: Sizes.size20,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white,
+                      width: Sizes.size4,
+                    ),
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+            ],
           ),
-          title: Text(
+          title: const Text(
             '신웅',
             style: TextStyle(
               fontWeight: FontWeight.w600,
             ),
           ),
-          subtitle: Text('Active now'),
-          trailing: Row(
+          subtitle: const Text('Active now'),
+          trailing: const Row(
             mainAxisSize: MainAxisSize.min,
             children: [
               FaIcon(
@@ -100,17 +138,78 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
           Positioned(
             bottom: 0,
             width: MediaQuery.of(context).size.width,
+            height: _isWriting ? 60 : 110,
             child: BottomAppBar(
               color: Colors.grey.shade50,
               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Expanded(
-                    child: TextField(),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: Sizes.size14,
+                      ),
+                      child: TextField(
+                        onTap: _onStartWriting,
+                        onSubmitted: _onSubmitted,
+                        decoration: const InputDecoration(
+                          hintText: 'Send a message...',
+                          fillColor: Colors.white,
+                          filled: true,
+                          contentPadding: EdgeInsets.all(Sizes.size14),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(
+                                Sizes.size28,
+                              ),
+                              topRight: Radius.circular(
+                                Sizes.size28,
+                              ),
+                              bottomLeft: Radius.circular(
+                                Sizes.size28,
+                              ),
+                            ),
+                          ),
+                          suffixIcon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              FaIcon(
+                                FontAwesomeIcons.faceSmile,
+                                color: Colors.black,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  Gaps.h20,
-                  Container(
-                    child: const FaIcon(
-                      FontAwesomeIcons.paperPlane,
+                  // Gaps.h10,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size14,
+                    ),
+                    child: GestureDetector(
+                      onTap: _onSendTap,
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey.shade300,
+                        ),
+                        child: const Center(
+                          child: Icon(
+                            FontAwesomeIcons.paperPlane,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ),
+                      ),
                     ),
                   )
                 ],
