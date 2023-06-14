0ea1ff67a1c70814762f87fd643b62e0b752756a
shinung-han <hanshinung@gmail.com>
2023-06-14 11:37:25 +0900

    24.2 signInWithEmailAndPassword

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone_2/constants/gaps.dart';
 import 'package:tiktok_clone_2/constants/sizes.dart';
+import 'package:tiktok_clone_2/features/authentication/view_models/login_view_model.dart';
 import 'package:tiktok_clone_2/features/authentication/widgets/form_button.dart';
 
-class LoginFormScreen extends StatefulWidget {
+class LoginFormScreen extends ConsumerStatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
-  State<LoginFormScreen> createState() => _LoginFormScreenState();
+  ConsumerState<LoginFormScreen> createState() => _LoginFormScreenState();
 }
 
-class _LoginFormScreenState extends State<LoginFormScreen> {
+class _LoginFormScreenState extends ConsumerState<LoginFormScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Map<String, String> formData = {};
@@ -20,7 +21,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
-        context.go('/home');
+        ref
+            .read(loginProvider.notifier)
+            .login(formData['email']!, formData['password']!, context);
       }
     }
   }
@@ -58,6 +61,7 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
               ),
               Gaps.v16,
               TextFormField(
+                obscureText: true,
                 decoration: const InputDecoration(
                   hintText: 'Password',
                 ),
@@ -76,8 +80,8 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
               Gaps.v28,
               GestureDetector(
                 onTap: _onSubmitTap,
-                child: const FormButton(
-                  disabled: false,
+                child: FormButton(
+                  disabled: ref.watch(loginProvider).isLoading,
                   text: 'Log in',
                 ),
               ),
