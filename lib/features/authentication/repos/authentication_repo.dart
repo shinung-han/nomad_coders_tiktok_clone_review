fbcbc16518b20bc187ce351bb28daeb085e0cd44
shinung-han <hanshinung@gmail.com>
2023-06-14 09:55:56 +0900

    add files

---
+++ lib/features/authentication/repos/authentication_repo.dart
@@ -0,0 +1,11 @@
+import 'package:auth/auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthenticationRepository {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  bool get isLoggedIn => user != null;
+  User? get user => _firebaseAuth.currentUser;
+}
+
+final authRepo = Provider((ref) => AuthenticationRepository());
